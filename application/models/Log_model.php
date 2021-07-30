4374fe141d077ed5c24095f935be8c2d1cb1965b
Alan Diaz <calix35@gmail.com>
2021-07-30 11:36:02 -0500

    Corrigiendo hora en Movil

--- application/models/Log_model.php
+++ application/models/Log_model.php
@@ -85,20 +85,17 @@ class Log_model extends CI_Model{
             $attendance=$query->result_array();
         }
         date_default_timezone_set('America/Monterrey');
-        $_attendance = [];
+        //$_attendance = [];
         foreach ($attendance as $att):
-            /*$dateu = mysql_to_unix($att['time']);
+            $dateu = mysql_to_unix($att['time']);
             if(date('I')==1) {
                 $dateu = gmt_to_local($dateu, "UP2", FALSE);
             }else
                 $dateu = gmt_to_local($dateu, "UP1", FALSE);
-                */
-            //$att['time']=unix_to_human($dateu); 
-            //$att['time2']=unix_to_human($dateu); 
-            $att['time'] = 'hola';
-            $_attendance[] = $att;           
+            $att['time']=unix_to_human($dateu); 
+            //$_attendance[] = $att;           
         endforeach;
-        return $_attendance;
+        return $attendance;
 
     }
 
