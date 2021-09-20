a6c78d23b2355a142e997fef8ec716df826e9573
Alan Diaz <calix35@gmail.com>
2021-09-20 09:16:41 -0500

    Tabla de Servicio Bascula

--- application/views/pages/showPlants.php
+++ application/views/pages/showPlants.php
@@ -11,7 +11,7 @@
 <main class="page-content">
   <div class="container-fluid">
     <section class="showPlants">
-        <table class="ui table">
+        <table style="margin-top: 30px;" class="ui table">
             <thead>
                 <tr>
                     <th>Planta</th>
