723ff45e7594d44f09bea15744b0e690208cca24
Alan Diaz <calix35@gmail.com>
2022-03-24 16:12:32 -0600

    Se agrega mica

--- application/controllers/Pedrera.php
+++ application/controllers/Pedrera.php
@@ -894,8 +894,8 @@ class Pedrera extends CI_Controller {
 				list($max1, $max2, $max3) = $this->Log_model->getTimeEntrada($lo['idLog']);
 				if($max1!=null && $max2!=null){
 					$this->excel->getActiveSheet()->setCellValue("K{$numrow}", $max1);
-					$this->excel->getActiveSheet()->setCellValue("L{$numrow}", $max3);
-					//$this->excel->getActiveSheet()->setCellValue("M{$numrow}", $max3);
+					$this->excel->getActiveSheet()->setCellValue("L{$numrow}", $max2);
+					$this->excel->getActiveSheet()->setCellValue("M{$numrow}", $max3);
 				}
 
 				// list($max1, $max2, $max3) = $this->Log_model->getTimes($lo['idLog'], 2, false);
