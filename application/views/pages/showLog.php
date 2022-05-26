177f1afd470a818d53d6108626c4693ed297628d
Alan Diaz <calix35@gmail.com>
2022-05-26 12:06:35 -0500

    Se corrige columna fecha

--- application/views/pages/showLog.php
+++ application/views/pages/showLog.php
@@ -238,7 +238,8 @@
         var mitabla = $('.tablaUsuarios').DataTable({
             "language": {
                 "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
-            }            
+            },
+            order: [[3, 'desc']],         
         });   
         
         //$('.tablaUsuarios').tablesort();        
