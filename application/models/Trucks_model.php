a45b8706f125231d24ca11364191824012cd039f
Alan Franco <alanfrancob97@gmail.com>
2022-09-04 15:37:23 -0500

    Refactorization
    Refactorized the Trucks_model.php file.

--- application/models/Trucks_model.php
+++ application/models/Trucks_model.php
@@ -1,43 +1,62 @@
 <?php
-class Trucks_model extends CI_Model{
 
-    public function __construct(){
-		$this->load->database();
-	}
+class Trucks_model extends CI_Model
+{
+    // constructor of this class
+    public function __construct()
+    {
+        $this->load->database();
+    }
 
-	public function getTrucks()
-	{	// TODO
-		$this->db->select('*');
-		$this->db->from("trucks");
+    // get all trucks from the database
+    public function getTrucks()
+    {
+        $this->db->select('*');
+        $this->db->from("trucks");
 
-		$query = $this->db->get();
-		
-		return $query->result_array();
-	}
+        $query = $this->db->get();
+        
+        return $query->result_array();
+    }
 
-    public function getTruck($plate){
-		$this->db->select('*');
-		$this->db->from("trucks, truckTypes");
-		$this->db->where('idTruck',$plate);
-		$this->db->where('trucks.idType=truckTypes.idType');
-		$query=$this->db->get();
-		if($query->num_rows()==1)
-			return $query->row();
-		return FALSE;
-	}
+    // --- THE METHODS BELLOW ARE NOT MINE --- Alan F.
 
-	public function existGPS($gps){
-		$this->db->select('*');
-		$this->db->from("gps");
-		$this->db->where('idGPS',$gps);
-		$query=$this->db->get();
-		if($query->num_rows()==1)
-			return TRUE;
-		return FALSE;
-	}
-	
-	public function addTruck($data){
+    // get the truck by the specified plate from the database
+    public function getTruck($plate)
+    {
+        $this->db->select('*');
+        $this->db->from("trucks, truckTypes");
+        $this->db->where('idTruck', $plate);
+        $this->db->where('trucks.idType = truckTypes.idType');
+
+        $query = $this->db->get();
+
+        if ( $query->num_rows() == 1 )
+            return $query->row();
+
+        return FALSE;
+    }
+
+    // adds a truck with the given data to the database
+    public function addTruck($data)
+    {
         $this->db->insert('trucks', $data);
     }
-    
+
+    // why is this even here??? don't touch it; might break something - Alan F.
+    public function existGPS($gps)
+    {
+        $this->db->select('*');
+        $this->db->from("gps");
+        $this->db->where('idGPS', $gps);
+
+        $query = $this->db->get();
+
+        if ( $query->num_rows() == 1 )
+            return TRUE;
+
+        return FALSE;
+    }
 }
+
+?>
